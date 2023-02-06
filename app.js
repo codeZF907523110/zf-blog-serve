c36f283bafa6a563ed420762d31df3002d88ed7b
流觞曲水 <907523110@qq.com>
2023-02-06 18:03:20 +0800

    跨域

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: zhangfeng16 zhangfeng16@shuidi-inc.com
  * @Date: 2022-12-26 15:20:22
  * @LastEditors: 流觞曲水 907523110@qq.com
- * @LastEditTime: 2023-02-06 17:55:38
+ * @LastEditTime: 2023-02-06 18:03:08
  * @FilePath: /zf-blog-server/app.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,7 +12,6 @@ const koaBody=require('koa-body')
 const static= require('koa-static')
 const fs = require('fs')
 const https = require('http')
-
 
 const blog = require('./router/blog.js')
 const label = require('./router/label.js')
@@ -45,6 +44,9 @@ app.use(async ctx => {
   ctx.body = 'Hello koa'
 });
 
-https.createServer(options, app.callback()).listen(3006,()=>{
+// https.createServer(options, app.callback()).listen(3006,()=>{
+//   console.log('run 3006')
+// },'0.0.0.0')
+app.listen(3006, () => {
   console.log('run 3006')
-},'0.0.0.0')
+}, '0.0.0.0')
