e0f7cd905e0af0a74d69f892cbb06e495ac35dc7
流觞曲水 <907523110@qq.com>
2023-02-06 16:10:03 +0800

    配置跨域

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: zhangfeng16 zhangfeng16@shuidi-inc.com
  * @Date: 2022-12-26 15:20:22
  * @LastEditors: 流觞曲水 907523110@qq.com
- * @LastEditTime: 2023-02-06 16:03:49
+ * @LastEditTime: 2023-02-06 16:09:52
  * @FilePath: /zf-blog-server/app.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -31,19 +31,13 @@ app.use(koaBody({
     maxFileSize: 200 * 1024 * 1024 // 设置上传文件大小最大限制，默认2M
   }
 }))
-app.use(
-  cors({
-    origin: function(ctx) { //设置允许来自指定域名请求
-      var allowCors = ['http://localhost:3006',  'http://www.zfblog.top']
-      return allowCors.indexOf(ctx.header.origin) > -1 ? ctx.header.origin : '';
-    },
-    maxAge: 5, //指定本次预检请求的有效期，单位为秒。
-    credentials: true, //是否允许发送Cookie
-    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法
-    allowHeaders: ['Content-Type', 'Authorization', 'Accept'], //设置服务器支持的所有头信息字段
-    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
-  })
-)
+app.use(cors({
+  origin:"*", // 允许来自指定域名请求
+  maxAge: 5, // 本次预检请求的有效期，单位为秒。
+  methods:['GET','POST'],  // 所允许的HTTP请求方法
+  alloweHeaders:['Conten-Type'], // 服务器支持的所有头信息字段
+  credentials: true // 是否允许发送Cookie
+}))
 //启动路由
 app.use(blog.routes()).use(label.routes()).use(leaveMessage.routes()).use(userInfo.routes())
 
