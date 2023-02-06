354dd4cdd2a334e5122047d343c571c582666a4b
流觞曲水 <907523110@qq.com>
2023-02-06 17:56:03 +0800

    跨域

--- app.js
+++ app.js
@@ -2,14 +2,12 @@
  * @Author: zhangfeng16 zhangfeng16@shuidi-inc.com
  * @Date: 2022-12-26 15:20:22
  * @LastEditors: 流觞曲水 907523110@qq.com
- * @LastEditTime: 2023-02-06 17:44:47
+ * @LastEditTime: 2023-02-06 17:55:38
  * @FilePath: /zf-blog-server/app.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-const cors = require('koa2-cors')
 const Koa=require('koa')
 const app = new Koa()
-app.use(cors())
 const koaBody=require('koa-body')
 const static= require('koa-static')
 const fs = require('fs')
@@ -20,6 +18,13 @@ const blog = require('./router/blog.js')
 const label = require('./router/label.js')
 const leaveMessage = require('./router/leaveMessage.js')
 const userInfo = require('./router/userInfo.js')
+
+app.use(async (ctx, next)=> {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Headers', 'Content-Type');
+  ctx.set('Access-Control-Allow-Methods', 'POST');
+  await next();
+})
 //  证书
 const options = {
   // key: fs.readFileSync('./static/SSL/9207781_zfblog.top.key'),
