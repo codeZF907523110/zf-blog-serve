047caea1daf0ea4ae5cf30e0e435edc3f69a6b2b
流觞曲水 <907523110@qq.com>
2023-02-06 16:43:13 +0800

    设置跨域

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: zhangfeng16 zhangfeng16@shuidi-inc.com
  * @Date: 2022-12-26 15:20:22
  * @LastEditors: 流觞曲水 907523110@qq.com
- * @LastEditTime: 2023-02-06 16:09:52
+ * @LastEditTime: 2023-02-06 16:42:15
  * @FilePath: /zf-blog-server/app.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,13 +14,7 @@ const https = require('http')
 const enforceHttps = require('koa-sslify')
 const app = new Koa()
 const cors = require('koa2-cors')
-app.use(cors({
-  origin:"*", // 允许来自指定域名请求
-  maxAge: 5, // 本次预检请求的有效期，单位为秒。
-  methods:['GET','POST'],  // 所允许的HTTP请求方法
-  alloweHeaders:['Conten-Type'], // 服务器支持的所有头信息字段
-  credentials: true // 是否允许发送Cookie
-}))
+app.use(cors())
 const blog = require('./router/blog.js')
 const label = require('./router/label.js')
 const leaveMessage = require('./router/leaveMessage.js')
