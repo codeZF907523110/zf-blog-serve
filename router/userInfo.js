75839679209e21e7f4daf958aeef4a6d8a2559a5
流觞曲水 <907523110@qq.com>
2023-02-07 18:09:43 +0800

    修改getuserinfo请求方法

--- router/userInfo.js
+++ router/userInfo.js
@@ -1,8 +1,8 @@
 /*
  * @Author: zhangfeng16 zhangfeng16@shuidi-inc.com
  * @Date: 2023-01-12 19:15:25
- * @LastEditors: zhangfeng16 zhangfeng16@shuidi-inc.com
- * @LastEditTime: 2023-01-18 11:18:03
+ * @LastEditors: 流觞曲水 907523110@qq.com
+ * @LastEditTime: 2023-02-07 18:07:06
  * @FilePath: /zf-blog-server/router/siteInformation.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,7 +16,7 @@ const { Blog } = require('../module/schema.js')
 const baseUrl = require('../config/baseUrl')
 
 // 获取作者信息
-router.post('/api/userInfo/getUserInfo', async (ctx) => {
+router.get('/api/userInfo/getUserInfo', async (ctx) => {
   const result = {
     userName: '张峰',
     signature: '世上本无难易,为之则易!',
