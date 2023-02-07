0a93a0df8c74f8f8a4dad3e40ac0b2e1a67b8b32
流觞曲水 <907523110@qq.com>
2023-02-07 11:34:59 +0800

    跨域

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: zhangfeng16 zhangfeng16@shuidi-inc.com
  * @Date: 2022-12-26 15:20:22
  * @LastEditors: 流觞曲水 907523110@qq.com
- * @LastEditTime: 2023-02-06 18:03:08
+ * @LastEditTime: 2023-02-07 11:34:39
  * @FilePath: /zf-blog-server/app.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,18 +17,13 @@ const blog = require('./router/blog.js')
 const label = require('./router/label.js')
 const leaveMessage = require('./router/leaveMessage.js')
 const userInfo = require('./router/userInfo.js')
-
-app.use(async (ctx, next)=> {
-  ctx.set('Access-Control-Allow-Origin', '*');
-  ctx.set('Access-Control-Allow-Headers', 'Content-Type');
-  ctx.set('Access-Control-Allow-Methods', 'POST');
-  await next();
-})
+const cors = require('koa2-cors')
 //  证书
 const options = {
   // key: fs.readFileSync('./static/SSL/9207781_zfblog.top.key'),
   // cert: fs.readFileSync('./static/SSL/9207781_zfblog.top.pem')
 }
+app.use(cors())
 // 静态路由
 app.use(static('./static'))
 app.use(koaBody({ 
