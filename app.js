60c71ddcd6d0db9dab1ce2aa0f7206ef3f1b7709
流觞曲水 <907523110@qq.com>
2023-02-10 14:54:22 +0800

    修改静态地址

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: zhangfeng16 zhangfeng16@shuidi-inc.com
  * @Date: 2022-12-26 15:20:22
  * @LastEditors: 流觞曲水 907523110@qq.com
- * @LastEditTime: 2023-02-07 11:34:39
+ * @LastEditTime: 2023-02-10 14:52:37
  * @FilePath: /zf-blog-server/app.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -25,7 +25,7 @@ const options = {
 }
 app.use(cors())
 // 静态路由
-app.use(static('./static'))
+app.use(static(__dirname + '/static'))
 app.use(koaBody({ 
   multipart: true,
   formidable: {
