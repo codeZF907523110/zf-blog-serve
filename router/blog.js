5e63d1ce5ae31c189a9263b73f39da865b5a314a
流觞曲水 <907523110@qq.com>
2023-02-10 17:05:40 +0800

    修改变量位置

--- router/blog.js
+++ router/blog.js
@@ -2,7 +2,7 @@
  * @Author: zhangfeng16 zhangfeng16@shuidi-inc.com
  * @Date: 2022-12-26 15:20:21
  * @LastEditors: 流觞曲水 907523110@qq.com
- * @LastEditTime: 2023-02-09 16:00:09
+ * @LastEditTime: 2023-02-10 17:05:22
  * @FilePath: /zf-blog-server/router/router.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -40,16 +40,17 @@ router.post('/api/blog/addOpenNum', async (ctx) => {
 // 条件查询博客
 router.post('/api/blog/getAllBlog', async (ctx) => {
   const form = ctx.request.body
+  let result, total
   if (!form.labels) delete form.labels
   try {
-    const result = await Blog.find({
+    result = await Blog.find({
       labels: form.labels ? form.labels : {$ne: null},
       _id: form._id ? form._id : {$ne: null}
     }).sort({_id: -1}).skip((form.pageNum - 1)*form.pageSize).limit(form.pageSize)
   } catch (error) {
   }
   try {
-    const total = await Blog.find({}).count()
+    total = await Blog.find({}).count()
   } catch (error) {
   }
   ctx.body = {
