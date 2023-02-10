6d6126514fd9512d281b40610934e2c05e47d127
流觞曲水 <907523110@qq.com>
2023-02-10 14:16:20 +0800

    修改静态资源域名

--- config/baseUrl.js
+++ config/baseUrl.js
@@ -2,8 +2,8 @@
  * @Author: zhangfeng16 zhangfeng16@shuidi-inc.com
  * @Date: 2023-01-16 19:08:33
  * @LastEditors: 流觞曲水 907523110@qq.com
- * @LastEditTime: 2023-01-30 17:02:01
+ * @LastEditTime: 2023-02-10 14:16:01
  * @FilePath: /zf-blog-server/config/baseUrl.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-module.exports = 'https://47.92.115.116:3006'
+module.exports = 'https://zf-blog-serve.top'
