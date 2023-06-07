cfb91f58f545596142fdf9ff208a790e6416ad31
zhangfeng16 <907523110@qq.com>
2023-06-07 10:48:56 +0800

    调试登录问题

--- router/login.js
+++ router/login.js
@@ -2,7 +2,7 @@
  * @Author: zhangfeng16 907523110@qq.com
  * @Date: 2023-05-06 11:10:14
  * @LastEditors: zhangfeng16 907523110@qq.com
- * @LastEditTime: 2023-05-25 14:48:19
+ * @LastEditTime: 2023-06-07 10:48:44
  * @FilePath: /zf-blog-serve/router/login.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -50,6 +50,7 @@ router.get('/api/github/callback', async (ctx) => {
       Authorization: `token ${access_token}`
     }
   })
+  console.log(res, '这是登录信息哦')
   const { secret } = require('../config/baseData')
   const payload = {user: res.data.login, icon: res.data.avatar_url}
   const token = jwt.sign(payload, secret, { expiresIn:  '24h' });
